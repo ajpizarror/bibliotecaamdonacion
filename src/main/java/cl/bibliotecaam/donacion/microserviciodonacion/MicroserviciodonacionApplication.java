7728ec81f314cbe63ec533dc0f85a6c8adf85449
VI-Alumno <VI-Alumno@duoc.acad>
2026-05-04 12:05:29 -0400

    primer commit

---
+++ src/main/java/cl/bibliotecaam/donacion/microserviciodonacion/MicroserviciodonacionApplication.java
@@ -0,0 +1,13 @@
+package cl.bibliotecaam.donacion.microserviciodonacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviciodonacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviciodonacionApplication.class, args);
+	}
+
+}
---
+++ src/main/java/cl/bibliotecaam/donacion/microserviciodonacion/model/Donacion.java
@@ -0,0 +1,4 @@
+package cl.bibliotecaam.donacion.microserviciodonacion.model;
+
+public class Donacion {
+}
