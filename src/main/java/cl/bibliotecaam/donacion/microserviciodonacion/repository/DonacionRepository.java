7869a4deef0bfc750de378bacc3d7682977ed6b3
MAGAQ <panamasado41@gmail.com>
2026-05-20 01:43:35 -0400

    Añadi en los query de las funciones unos concat y seguridad en el uso de mayusculas y minusculas. MS Listo para atacar >:D

--- src/main/java/cl/bibliotecaam/donacion/microserviciodonacion/repository/DonacionRepository.java
+++ src/main/java/cl/bibliotecaam/donacion/microserviciodonacion/repository/DonacionRepository.java
@@ -8,13 +8,13 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface DonacionRepository extends JpaRepository<Donacion, Long> {
-    @Query("SELECT d FROM Donacion d WHERE d.numrun = :numrun")
+    @Query("SELECT d FROM Donacion d WHERE UPPER(d.numrun) LIKE UPPER(CONCAT('%', :numrun, '%'))")
     List<Donacion> findByNumrun(@Param("numrun") Long numrun);
 
-    @Query("SELECT d FROM Donacion d WHERE d.pnombre = :nombre")
+    @Query("SELECT d FROM Donacion d WHERE UPPER(d.pnombre) LIKE UPPER(CONCAT('%',:nombre,'%'))")
     List<Donacion> findByPnombre(@Param("nombre") String nombre);
 
-    @Query("SELECT d FROM Donacion d WHERE d.appaterno = :apellido")
+    @Query("SELECT d FROM Donacion d WHERE UPPER(d.appaterno) = UPPER(CONCAT('%',:apellido,'%'))")
     List<Donacion> findByAppaterno(@Param("apellido") String apellido);
 
 }
