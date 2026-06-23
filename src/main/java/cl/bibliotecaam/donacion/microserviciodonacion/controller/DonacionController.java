2261790493a108acd0f915d674d6c8a9149fef99
MAGAQ <panamasado41@gmail.com>
2026-06-23 05:39:46 -0400

    Añadi el boton de autenticacion. tambien active la seguridad para el api-gateway, MS listo pal mambo >:D

--- src/main/java/cl/bibliotecaam/donacion/microserviciodonacion/controller/DonacionController.java
+++ src/main/java/cl/bibliotecaam/donacion/microserviciodonacion/controller/DonacionController.java
@@ -10,6 +10,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -27,6 +28,7 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@SecurityRequirement(name = "bearerAuth")
 @RestController
 @RequestMapping("/api/bibliotecaam/donacion")
 @RequiredArgsConstructor
